f71fe43a2a435cfb16aa70a86538f2d208ba4168
Samit <Samit.prsl@gmail.com>
2024-05-28 23:23:10 +0530

    some more programs here

--- largeShit.cpp
+++ largeShit.cpp
@@ -2,29 +2,27 @@
 namespace devSpace {
     template<class Type>
     class Arr {
-        public:
+        private:
             Type *arr;
             int size;
         public:
             Arr(int _size){
-                this->size = _size;
+                this->size = _size;//size = _size
                 arr = new Type[size];
             }
-            friend std::istream& operator>>(std::istream& input,Arr<Type>& A){
+            friend void operator>>(std::istream& input,Arr<Type>& A){
                 for(int i=0;i<A.size;i++){
                     std::cout<<"Enter elements at index "<<i<<std::endl;
                     input >> A.arr[i];
                 }
-                return input;
             }
-            friend std::ostream& operator<<(std::ostream& output,Arr<Type>& A){
+            friend void operator<<(std::ostream& output,Arr<Type>& A){
                 for(int i=0;i<A.size;i++){
                     output << A.arr[i] << " ";
                 }
                 output<<std::endl;
-                return output;
             }
-            bool operator==(const Arr& A){
+            bool operator==(const Arr<Type>& A){
                 for(int i=0;i<size;i++){
                     if(arr[i]!=A.arr[i])
                         return false;
@@ -32,14 +30,39 @@ namespace devSpace {
                 return true;
             }
     };
+
+    class Exp{
+        private:
+            std::string errMsg = "";
+        public:
+            Exp(){
+                errMsg = "array out of bound";
+            }
+            std::string showErr() const {
+                return errMsg;
+            }
+    };
+}
+
+void sizeValidation(int size){
+    if(size > 5)
+        throw devSpace::Exp();
 }
 
 int main(){
-    devSpace::Arr<double> A1(5);
-    std::cin>>A1;
-    std::cout<<A1;
-    devSpace :: Arr<double> A2(5);
-    std::cin>>A2;
-    std::cout<<A2;
+    std::cout<<"Enter size of array 1"<<std::endl;
+    int size1;
+    std::cin>>size1;
+    try{
+        sizeValidation(size1);
+        devSpace::Arr<double> A1(size1);
+        std::cin>>A1;
+        std::cout<<A1;
+    }
+    catch(const devSpace::Exp& E){
+        std::string printMsg = E.showErr();
+        std::cout<<printMsg<<std::endl;
+    }
+    
     return 0;
 }
